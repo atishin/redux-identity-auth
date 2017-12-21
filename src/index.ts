f5fa0567f3de4e6549795941279a2a6192baa163
Anton Tishin <atishin93@gmail.com>
2017-12-21 12:49:47 +0600

    publish

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import './auth.service';
+import './auth.service-with-roles';
+import './initial.states';
+import './ng-redux-identity-auth.module';
+import './token.endpoint';
+import './user.actions';
+import './user.reducer';
+import './user.state';
+
+
+export * from './auth.service';
+export * from './auth.service-with-roles';
+export * from './initial.states';
+export * from './ng-redux-identity-auth.module';
+export * from './token.endpoint';
+export * from './user.actions';
+export * from './user.reducer';
+export * from './user.state';
